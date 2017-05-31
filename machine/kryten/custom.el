c2da6698bc3530b22f2f13b35eeb93794f68191a
w-vi <vilibald@wvi.cz>
2017-05-31 12:37:26 +0200

    kryten

---
+++ machine/kryten/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (editorconfig diminish jump-char guide-key browse-kill-ring ido-ubiquitous ido-at-point ido-vertical-mode whitespace-cleanup-mode dired-details visual-regexp slime htmlize move-text yaml-mode web-mode vlf undo-tree tabbar smex smartparens skewer-mode request-deferred py-isort py-autopep8 org multiple-cursors magit-push-remote magit-gh-pulls magit-find-file magit-filenotify lua-mode iy-go-to-char go-snippets go-projectile gnuplot-mode gnuplot gist ggtags flycheck-package flx-ido fill-column-indicator expand-region ess-R-object-popup elpy edit-server ecb dockerfile-mode company-web company-quickhelp company-jedi company-go coffee-mode bison-mode apib-mode ace-jump-mode)))
+ '(tabbar-separator (quote (0.5))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
