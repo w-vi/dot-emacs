c0cdb2fd77978b0fb5cb85e47378a70397e5df6b
w-vi <vilibald@wvi.cz>
2017-12-03 15:19:50 +0100

    Add search engines

--- settings/setup-engine.el
+++ settings/setup-engine.el
@@ -3,10 +3,26 @@
 
 ;; Github
 (defengine github
-  "https://github.com/search?ref=simplesearch&q=%s")
+  "https://github.com/search?ref=simplesearch&q=%s"
+  :keybinding "g")
 
 ;; duck duck go
 (defengine duckduckgo
-  "https://duckduckgo.com/?q=%s")
+  "https://duckduckgo.com/?q=%s"
+  :keybinding "d")
+
+(defengine rfcs
+  "http://pretty-rfc.herokuapp.com/search?q=%s")
+
+(defengine stack-overflow
+  "https://stackoverflow.com/search?q=%s")
+
+(defengine wikipedia
+  "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+  :keybinding "w"
+  :docstring "Searchin' the wikis.")
+
+(defengine youtube
+  "http://www.youtube.com/results?aq=f&oq=&search_query=%s")
 
 (provide 'setup-engine)
