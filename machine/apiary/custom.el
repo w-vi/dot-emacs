320b84dfbd684611c0f548af5b28fd4a27556e4e
w-vi <vilibald@wvi.cz>
2018-01-11 16:48:50 +0100

    js-indent in apiary

--- machine/apiary/custom.el
+++ machine/apiary/custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(coffee-indent-like-python-mode nil)
  '(coffee-tab-width 4)
+ '(js-indent-level 2)
+ '(js2-missing-semi-one-line-override nil)
  '(package-selected-packages
    (quote
     (jump-char editorconfig multiple-cursors expand-region ggtags undo-tree iy-go-to-char tabbar guide-key browse-kill-ring ido-ubiquitous ido-at-point ido-vertical-mode fill-column-indicator whitespace-cleanup-mode dired-details visual-regexp slime ace-jump-mode htmlize move-text yaml-mode web-mode vlf smex smartparens skewer-mode request-deferred py-isort py-autopep8 org magit-push-remote magit-gh-pulls magit-find-file magit-filenotify lua-mode go-snippets go-projectile gnuplot-mode gnuplot gist flycheck-package flx-ido ess-R-object-popup elpy edit-server ecb dockerfile-mode direx company-web company-tern company-quickhelp company-jedi company-go coffee-mode apib-mode)))
