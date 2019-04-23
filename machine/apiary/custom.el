dbc52f8a217859110f98e18a87a8d122d1e214ee
w-vi <vilibald@wvi.cz>
2019-04-23 15:13:46 +0200

    apiary: use Liberation Mono 12pt as default font

--- machine/apiary/custom.el
+++ machine/apiary/custom.el
@@ -3,18 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(coffee-indent-like-python-mode nil)
  '(coffee-tab-width 4)
- '(js-indent-level 2)
+ '(column-number-mode t)
+ '(js-indent-level 2 t)
  '(js2-missing-semi-one-line-override nil)
  '(package-selected-packages
    (quote
     (wrap-region w3m engine-mode docker diminish company-terraform graphviz-dot-mode org-page jump-char editorconfig multiple-cursors expand-region ggtags undo-tree iy-go-to-char tabbar guide-key browse-kill-ring ido-ubiquitous ido-at-point ido-vertical-mode fill-column-indicator whitespace-cleanup-mode dired-details visual-regexp slime ace-jump-mode htmlize move-text yaml-mode web-mode vlf smex smartparens request-deferred py-isort py-autopep8 org magit-push-remote magit-gh-pulls magit-find-file magit-filenotify lua-mode go-snippets go-projectile gnuplot-mode gnuplot gist flycheck-package flx-ido elpy ecb dockerfile-mode direx company-web company-tern company-quickhelp company-jedi company-go coffee-mode apib-mode)))
+ '(show-paren-mode t)
  '(tabbar-separator (quote (0.5)))
+ '(tool-bar-mode nil)
  '(vlf-application (quote dont-ask)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 120 :width normal)))))
